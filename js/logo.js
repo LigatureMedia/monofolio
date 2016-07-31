644c0c3c5fdb9995450c3d1f6b0857684561f71b
Ari Stassinopoulos <aris@qsysmine.tk>
2016-07-31 21:15:30 +0000

    Init Commit

---
+++ js/logo.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    //set icon
+    var setIcon = function(href) {
+        document.querySelector("#favicon").href = href;
+    }
+    //set logo
+    var colours = ["#ffaa3e", "#1dc116", "#b43d3d"];
+    var $el = document.querySelector('.head>a');
+    var txt = $el.text;
+    var chars = txt.split("");
+    var conString = "";
+    chars.forEach(function(char) {
+        var colour = colours[Math.floor(Math.random() * colours.length)];
+        if(char == "M") {
+            setIcon("/icons/m_" + colour.split("#")[1] + ".ico");
+        }
+        conString += "<span style='color: " + colour + "'>" + char + "</span>"
+    });
+    console.log(conString);
+    $el.innerHTML = conString;
+});
